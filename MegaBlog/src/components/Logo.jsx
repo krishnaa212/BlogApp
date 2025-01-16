e5e2f5abf275c0c70f464ebfaa181b107e439e84
krishnaa212 <krishnadidwania17@gmail.com>
2025-01-16 22:39:05 +0530

    components

---
+++ MegaBlog/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = "100px"}) {
+  return (
+    <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/chat-app-icon-logo-design-template-770ca6add87165646ba67d1a36dfee4e.jpg?ts=1600190289" alt="LOGO" />
+  )
+}
+
+export default Logo
