816a331c0a6a42d5640fa3b74551d651f421e7d3
krishnaa212 <krishnadidwania17@gmail.com>
2025-01-17 23:08:48 +0530

    setting up components

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,6 +3,11 @@ import Footer from "./Footer/Footer"
 import Container from "./Container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
+import Button from "./Button"
+import Select from "./Select"
+import PostCard from "./PostCard"
+import Input from "./Input"
+import Login from "./Login"
 
 export {
   Header,
@@ -10,4 +15,9 @@ export {
   Container,
   Logo,
   LogoutBtn,
+  Button,
+  Select,
+  PostCard,
+  Input,
+  Login
 }
